0996426b3405ce67fc8950689fbb76df4160541d
Lucas Dias <lucas.dias@getrak.com.br>
2022-03-28 21:17:48 -0300

    salvando

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+import { GenreResponseProps } from "../Model/GenreResponseProps";
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  onChangeGenre: (id: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar({ genres, onChangeGenre, selectedGenreId }: SideBarProps) {
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onChangeGenre(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
